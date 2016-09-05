1173c053a9a0521985ef726fe0c8dbaba11d97da
Zangdarr <alexandre.verkyndt@gmail.com>
2016-09-05 10:15:21 +0200

    main of the project with a first base.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<tools.h>
+
+using namespace std;
+
+int 
+
+int main(){
+
+    std::string action;
+
+    std::cout<< "Welcome on ZPlanner\n";
+    std::cout<< "What action do you want to realize?\n";
+    std::cout<< "1 - print listed task\n";
+    std::cout<< "2 - add a new task\n";
+    std::cout<< "Enter the task id :\n";
+    std::cin>> action;
+    std::cin.ignore();
+    
+    std::cin.get();
+}

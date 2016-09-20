3aa22055b5f8b66c3eeae6d2b0f3475b11217cab
Zangdarr <alexandre.verkyndt@gmail.com>
2016-09-20 13:27:44 +0200

    Coming back to a minimalistic compilable version that will be used as a base for this project.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-int add-task(); 
+void action_selector(std::string);
+void add_task(); 
+void print_task();
 
 int main(){
 
     std::string action;
 
-    std::cout<< "Welcome on ZPlanner\n";
+    std::cout<< "Welcome on your ZPlanner\n";
     std::cout<< "What action do you want to realize?\n";
     std::cout<< "1 - print listed task\n";
     std::cout<< "2 - add a new task\n";
@@ -16,5 +18,27 @@ int main(){
     std::cin>> action;
     std::cin.ignore();
     
+    action_selector(action);
+    
     std::cin.get();
+}
+
+void action_selector(std::string action){
+    switch(std::stoi(action)){
+        case 1: { print_task();
+                    break;}
+        case 2: { add_task();
+                    break;}
+        default:  std::cout<< "Unaffected id entered.";
+                  break;
+    }
+}
+
+void add_task(){
+    std::cout<< "function add_task()";
+
+}
+
+void print_task(){
+    std::cout<< "function print_task()";
 }

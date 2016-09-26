b8275dde709cb88db7ed50c93e05a8406ee2ca1a
Zangdarr <alexandre.verkyndt@gmail.com>
2016-09-26 14:29:02 +0200

    Update of print and add.
    Now they consider the current day and not today only.

--- main.cpp
+++ main.cpp
@@ -57,16 +57,16 @@ void add_task(){
     std::string new_task;
     std::getline(std::cin, new_task); //get the task from the user
 
-    std::string today_year = s_config.filename_today.substr(6,2);
-    std::string today_month = s_config.filename_today.substr(0,2);
-    std::string today_folder = "database/20" + today_year + "_" + today_month; // determine the folder of the day
+    std::string current_year = s_config.filename_current.substr(6,2);
+    std::string current_month = s_config.filename_current.substr(0,2);
+    std::string current_folder = "database/20" + current_year + "_" + current_month; // determine the current folder
 
-    std::string command_line = "[ ! -d " + today_folder + " ] && mkdir " + today_folder; 
+    std::string command_line = "[ ! -d " + current_folder + " ] && mkdir " + current_folder; 
     std::system(command_line.c_str()); // creation of the folder if needed
     
-    std::string today_path = today_folder + "/" + s_config.filename_today; //
+    std::string current_path = current_folder + "/" + s_config.filename_current; //
 
-    std::ofstream out(today_path, std::fstream::app | std::fstream::out | std::fstream::in);
+    std::ofstream out(current_path, std::fstream::app | std::fstream::out | std::fstream::in);
     out << new_task;
     out << "\n";
 
@@ -77,14 +77,13 @@ void add_task(){
 void print_task(){
     std::cout<< "TODO :\n";
     
-    std::string today_year = s_config.filename_today.substr(6,2);
-    std::string today_month = s_config.filename_today.substr(0,2);
-    std::string today_folder = "database/20" + today_year + "_" + today_month; // determine the folder of the day
+    std::string current_year = s_config.filename_current.substr(6,2);
+    std::string current_month = s_config.filename_current.substr(0,2);
+    std::string current_folder = "database/20" + current_year + "_" + current_month; // determine the folder of the day
 
-    std::string today_path = today_folder + "/" + s_config.filename_today; //
+    std::string current_path = current_folder + "/" + s_config.filename_current; //
 
-   
-    std::ifstream file(today_path);
+    std::ifstream file(current_path);
     std::string str_tmp;
 
     while(std::getline(file, str_tmp)){
